5126f09c0a0e2ea3fe40495561c67f9a9ac6bdb2
Geo Min <geomin12@amd.com>
2025-04-03 16:28:31 -0700

    Testing releases
    * Adding initial test release file
    
    * Format fix
    
    * Format fix
    
    * It appears ordering matters
    
    * It appears ordering matters
    
    * ok if statement not a fan there
    
    * ok if statement not a fan there
    
    * Adding matrix generation
    
    * Fixing permissions
    
    * adding python
    
    * Correcting json
    
    * simplifying matrix
    
    * Fixing tar
    
    * Adding mkdir
    
    * Adding release notes
    
    * testing with ubuntu
    
    * Testing this
    
    * Missing quotes
    
    * Trying this
    
    * Trying this
    
    * Writing to markdown
    
    * Okay this should work
    
    * Adding cancel step
    
    * markdown cleanup
    
    * Adding cleanup and docs

---
+++ build_tools/configure_release_matrix.py
@@ -0,0 +1,24 @@
+import os
+import json
+from typing import Mapping
+from configure_ci import set_github_output
+
+# This file helps generate a release test matrix for test_release_packages.yml
+
+
+def main(args):
+    assets = json.loads(args.get("asset_files"))
+    release_matrix = []
+    for asset in assets:
+        asset_name = asset.get("name", "")
+        # for now, we can only run tests on gfx94X, so adding hard-coded value
+        if "gfx94X" in asset_name:
+            release_matrix.append(asset_name)
+
+    set_github_output({"release_matrix": json.dumps(release_matrix)})
+
+
+if __name__ == "__main__":
+    args = {}
+    args["asset_files"] = os.environ.get("ASSET_FILES", "[]")
+    main(args)
