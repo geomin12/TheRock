6fb5bf0c12745b05d24888eb06d0ac7153d7b1d3
geomin12 <geomin12@amd.com>
2025-04-03 14:30:34 -0700

    Adding matrix generation

---
+++ build_tools/configure_release_matrix.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+def set_github_output(d: Mapping[str, str]):
+    """Sets GITHUB_OUTPUT values.
+    See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/passing-information-between-jobs
+    """
+    print(f"Setting github output:\n{d}")
+    step_output_file = os.environ.get("GITHUB_OUTPUT", "")
+    if not step_output_file:
+        print("Warning: GITHUB_OUTPUT env var not set, can't set github outputs")
+        return
+    with open(step_output_file, "a") as f:
+        f.writelines(f"{k}={v}" + "\n" for k, v in d.items())
+
+def main(args):
+    assets = json.loads(args.get("asset_files"))
+    release_matrix = []
+    for asset in assets:
+        asset_name = asset.get("name", "")
+        # for now, we can only run tests on mi300 (gfx94X), so hard-coded value here
+        if "gfx94X" in asset_name:
+            release_matrix.append({"name": asset_name})
+            
+    set_github_output({
+        "release_matrix": json.dumps(release_matrix)
+    })
+
+if __name__ == "__main__":
+    args = {}
+    args["asset_files"] = os.environ.get("ASSET_FILES", "[]")
+    main(args)
